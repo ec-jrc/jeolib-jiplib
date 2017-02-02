cc58394af0ac9343499d415105401d9711b4c60b
Pierre Soille <Pierre.Soille@jrc.ec.europa.eu>
2017-02-02 18:44:24 +0100

    Fine tuning.

--- src/fun_nondestructive2method.py
+++ src/fun_nondestructive2method.py
@@ -194,7 +194,7 @@ if __name__ == "__main__":
    main(sys.argv[1:])
 
 
-# cat /home/soillpi/work/jip20161114/mia//core/c/mialib_*.h | grep '^extern IMAGE \*[^\*]'  > mialib_image_type
+# cat ../..//mia//core/c/mialib_*.h | grep '^extern IMAGE \*[^\*]'  > mialib_image_type
 # python fun_nondestructive2method.py  -i mialib_image_type -o fun_nondestructive2method
 # to automatically insert content of fun2method in jim.h within placeholder //start insert from fun2method -> //end insert from fun2method
 # sed -i -ne '/\/\/start insert from fun2method/ {p; r fun2method.h' -e ':a; n; /\/\/end insert from fun2method/ {p; b}; ba}; p' jim.h
