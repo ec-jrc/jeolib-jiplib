73150bd4ea066433acafaed8f4e16106f4f36e0d
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2018-07-11 14:50:31 +0200

    added pytest_classifyOgr and pytest_crop

---
+++ test/pytest_classifyOgr.py
@@ -0,0 +1,55 @@
+# pytest_classifyOgr.py: classify vector dataset
+# History
+# 2018/03/07 - Created by Pieter Kempeneers (pieter.kempeneers@ec.europa.eu)
+# Change log
+
+
+import argparse
+import os
+import math
+import jiplib as jl
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-input","--input",help="Path of the input raster dataset",dest="input",required=True,type=str)
+parser.add_argument("-vector","--vector",help="Path of the sample vector dataset with labels",dest="vector",required=True,type=str)
+parser.add_argument("-model","--model",help="Path of the model output filename used for training",dest="model",required=True,type=str)
+parser.add_argument("-output","--output",help="Path of the classification output raster dataset",dest="output",required=True,type=str)
+parser.add_argument("-classifier","--classifier",help="classifier (svm, ann)",dest="classifier",required=False,type=str,default="svm")
+args = parser.parse_args()
+
+try:
+    print("createJim")
+    jim=jl.createJim({'filename':args.input})
+    print("createVector")
+    sample=jl.createVector();
+    print("open vector",args.vector)
+    sample.open(args.vector)
+    print("extractOgr")
+    training=jim.extractOgr(sample,{'output':'training','oformat':'Memory','copy':'label'})
+    test=jim.extractOgr(sample,{'output':'test','oformat':'Memory','copy':'None'})
+    if args.classifier == 'svm':
+        #SVM classification
+        print("training")
+        training.train({'method':args.classifier,'label':'label','model':args.model})
+        print("classification")
+        vclass=test.classify({'output':args.output,'co':'OVERWRITE=YES','model':args.model,'method':args.classifier})
+        print("write")
+        vclass.write()
+        vclass.close()
+    else:
+        #ANN classification
+        print("training")
+        training.train({'method':args.classifier,'label':'label','model':args.model})
+        print("classification")
+        vclass=test.classify({'output':args.output,'co':'OVERWRITE=YES','model':args.model,'method':args.classifier})
+        print("write")
+        vclass.write()
+        vclass.close()
+        sample.close()
+    test.close()
+    training.close()
+    jim.close()
+    print("Success: classify")
+except:
+    print("Failed: classify")
+
---
+++ test/pytest_crop.py
@@ -0,0 +1,48 @@
+# pytest_crop.py: crop
+# History
+# 2018/03/01 - Created by Pieter Kempeneers (pieter.kempeneers@ec.europa.eu)
+# Change log
+
+
+import argparse
+import os
+import math
+import jiplib as jl
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-input","--input",help="Path of the raster dataset",dest="input",required=True,type=str)
+parser.add_argument("-extent","--extent",help="Path of the vector dataset",dest="extent",required=False,type=str)
+parser.add_argument("-output","--output",help="Path of the output vector dataset",dest="output",required=False,type=str)
+parser.add_argument("-nodata","--nodata",help="nodata value to put in cut region",dest="nodata",required=False,default=0,type=int)
+parser.add_argument("-cut_to_cutline","--cut_to_cutline",help="Crop the extent of the target dataset to the extent of the cutline, setting the outside area to nodata",dest="cut_to_cutline",required=False,type=bool,default=False)
+parser.add_argument("-cut_in_cutline","--cut_in_cutline",help="Crop the extent of the target dataset to the extent of the cutline, setting the inner area to nodata",dest="cut_in_cutline",required=False,type=bool,default=False)
+args = parser.parse_args()
+
+try:
+    jim0=jl.createJim({'filename':args.input})
+    rules=['centroid','min','max','mean','stdev']
+    jlv=jl.createVector(args.extent);
+    if os.path.basename(args.extent)=='nuts_italy.sqlite':
+        if args.cut_to_cutline:
+            print("cut out")
+            jim_milano=jim0.cropOgr(jlv,{'ln':'milano','crop_to_cutline':True,'nodata':args.nodata,'align':True})
+            jim_lodi=jim0.cropOgr(jlv,{'ln':'lodi','crop_to_cutline':True,'nodata':args.nodata,'align':True})
+        elif args.cut_in_cutline:
+            print("cut within")
+            # to keep region
+            jim_milano=jim0.cropOgr(jlv,{'ln':'milano','crop_in_cutline':True,'nodata':args.nodata,'align':True})
+            jim_lodi=jim0.cropOgr(jlv,{'ln':'lodi','crop_in_cutline':True,'nodata':args.nodata,'align':True})
+        milanofn=os.path.join(os.path.dirname(args.output),'milano.tif')
+        print("write")
+        lodifn=os.path.join(os.path.dirname(args.output),'lodi.tif')
+        jim_milano.write({'filename':milanofn})
+        jim_milano.close()
+        jim_lodi.write({'filename':lodifn})
+        jim_lodi.close()
+    else:
+        jim0.cropOgr(jlv,{'align':True}).write({'filename':args.output}).close()
+    jim0.close()
+    jlv.close()
+    print("Success: crop")
+except:
+    print("Failed: crop")
