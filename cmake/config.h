c94ebe4e91656d6b497a421b5d5f5f70e89f3d30
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2022-02-24 01:02:33 +0100

    update version to 1.0.6

--- cmake/config.h
+++ cmake/config.h
@@ -1,6 +1,6 @@
 // the configured options and settings for jiplib
 #define JIPLIB_VERSION_MAJOR 1
 #define JIPLIB_VERSION_MINOR 0
-#define JIPLIB_VERSION_PATCH 0
+#define JIPLIB_VERSION_PATCH 6
 #define VERSION 1
 #define CLASSIF 0
