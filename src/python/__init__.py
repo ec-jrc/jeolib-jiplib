121bed4159403cc7b27d8e0e9cec88c95e3a05d3
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2023-09-11 10:51:21 +0200

    set version to 1.1.3

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -23,4 +23,4 @@ from __future__ import absolute_import
 from .jiplib import *
 
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
--- src/python/setup.py
+++ src/python/setup.py
@@ -27,7 +27,7 @@ from setuptools import find_packages
 
 setup(
     name='jiplib',
-    version='1.1.2',
+    version='1.1.3',
     author='Pieter Kempeneers',
     author_email='pieter.kempeneers@.ec.europa.eu',
     url='https://jeodpp.jrc.ec.europa.eu/apps/gitlab/JIPlib/jiplib',
