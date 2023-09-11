66ff105bae7e038e337886d0fc1e56a855d1105f
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2023-09-11 10:45:44 +0200

    requiring Python 3.8 instead of 3.9

--- src/python/setup.py
+++ src/python/setup.py
@@ -53,7 +53,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.8',
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.8',
 )
