06f7ef63728c1f10e240c8b8ef1e2db40bf5f10a
Pieter Kempeneers <kempenep@gmail.com>
2026-05-06 11:20:25 +0200

    updated version in doc and python

--- src/python/setup.py
+++ src/python/setup.py
@@ -27,7 +27,7 @@ from setuptools import find_packages
 
 setup(
     name='jiplib',
-    version='1.1.7',
+    version='1.2.0',
     author='Pieter Kempeneers',
     author_email='pieter.kempeneers@ec.europa.eu',
     url='https://jeodpp.jrc.ec.europa.eu/apps/gitlab/JIPlib/jiplib',
