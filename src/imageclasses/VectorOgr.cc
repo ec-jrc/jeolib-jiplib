d75cc4b3442d457d028e3ea5e750278aa3101be8
Pieter Kempeneers <kempenep@gmail.com>
2026-07-24 15:40:29 +0200

    enforce local miallib build and no const OGRSpatialReference in pushLayer

--- src/imageclasses/VectorOgr.cc
+++ src/imageclasses/VectorOgr.cc
@@ -475,7 +475,7 @@ void VectorOgr::close(void)
 }
 
 ///Create a layer
-OGRErr VectorOgr::pushLayer(const std::string& layername, const OGRSpatialReference* theSRS, const OGRwkbGeometryType& geometryType, char** papszOptions){
+OGRErr VectorOgr::pushLayer(const std::string& layername, OGRSpatialReference* theSRS, const OGRwkbGeometryType& geometryType, char** papszOptions){
   if( !m_gds->TestCapability( ODsCCreateLayer ) ){
     // std::ostringstream errorStream;
     // errorStream << "Error: Test capability to create layer " << layername << " failed (1)" << std::endl;
